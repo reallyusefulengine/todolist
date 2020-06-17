a580d390a48e8498f1316d7a81208a1a5d448dc1
reallyusefulengine <will.jh75@gmail.com>
2020-06-17 20:38:38 +1000

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import TodoList from "../src/components/TodoList";
 import TodoInput from "./components/TodoInput";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const id = uuidv4();
   return (
     <section>
       <TodoInput />
       <TodoList />
+      {id}
     </section>
   );
 }
