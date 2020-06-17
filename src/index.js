897723bb230b2df2b1d0e7cbb86df5523614afac
reallyusefulengine <will.jh75@gmail.com>
2020-06-17 13:24:40 +1000

    updted dependencies

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../src/dependencies/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
